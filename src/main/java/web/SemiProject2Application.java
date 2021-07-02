8e789f7e64219b3e8b7cb25889f561a3533d581b
cds1219 <cds1219_@naver.com>
2021-07-02 14:10:38 +0900

    1st

---
+++ src/main/java/web/SemiProject2Application.java
@@ -0,0 +1,13 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProject2Application.class, args);
+	}
+
+}
