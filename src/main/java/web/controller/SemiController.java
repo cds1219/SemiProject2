401653331f89fdfd799ac592e00fd379b708fd5b
cds1219 <cds1219_@naver.com>
2021-07-03 22:44:46 +0900

    백업용

---
+++ src/main/java/web/controller/SemiController.java
@@ -0,0 +1,12 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import web.service.SemiService;
+
+public class SemiController {
+
+	@Autowired
+	SemiService semiService;
+	
+}
